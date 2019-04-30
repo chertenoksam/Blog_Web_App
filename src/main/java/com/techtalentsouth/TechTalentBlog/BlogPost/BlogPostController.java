73debc8b32c3357b2438e71bb808643545aa4e52
Maria Podolyak <mariapodolyak@Air-1803.attlocal.net>
2019-04-30 16:10:04 -0400

    version 1.0

---
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
@@ -0,0 +1,55 @@
+package com.techtalentsouth.TechTalentBlog.BlogPost;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BlogPostController<posts> {
+	
+	Log log = LogFactory.getLog(BlogPostController.class);
+	
+	@Autowired
+	private BlogPostRepository blogPostRepository;
+	private static List<BlogPost> posts = new ArrayList<>();
+	
+	@GetMapping(value="/blogpost/new")
+	public String index(BlogPost blogPost, Model model) {
+		model.addAttribute("posts", posts);
+		log.error("index");
+		return "blogpost/index.html"; 
+	}
+	
+	private BlogPost blogPost;
+	
+	@PostMapping(value = "/blogpost/new")
+	public String create(BlogPost blogPost, Model model) {
+		blogPostRepository.save(blogPost);
+		posts.add(blogPost);
+		model.addAttribute("title", blogPost.getTitle());
+		model.addAttribute("author", blogPost.getAuthor());
+		model.addAttribute("blogEntry", blogPost.getBlogEntry());
+		log.error("result");
+		return "blogpost/result.html";
+	        }
+    @RequestMapping(value = "/blogpost/{id}", method = RequestMethod.DELETE)
+    public String deletePostWithId(@PathVariable Long id,
+                                   BlogPost blogPost) {
+
+        blogPostRepository.deleteById(id);
+        return "blogpost/index";
+
+    }
+	
+}
